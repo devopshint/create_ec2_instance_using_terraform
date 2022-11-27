07d4f2664ed3ccfa5c70dd0e2d47d8c0da959cb5
devopshint <83763090+devopshint@users.noreply.github.com>
2022-11-27 15:32:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 
-#provider "aws" {
+provider "aws" {
     #access_key = "${var.access_key}"
     #secret_key = "${var.secret_key}"
   #  region = "ap-south-1"
-#}
+}
 
 
 resource "aws_instance" "Demo" {
