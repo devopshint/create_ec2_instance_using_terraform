8572e5ed0aa1eeab1b19a78613b6bbc6251a8673
devopshint <83763090+devopshint@users.noreply.github.com>
2022-11-27 15:30:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
-provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
+
+
+#provider "aws" {
+    #access_key = "${var.access_key}"
+    #secret_key = "${var.secret_key}"
+  #  region = "ap-south-1"
+#}
 
 
 resource "aws_instance" "Demo" {
